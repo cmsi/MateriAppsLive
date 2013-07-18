ba1b3c8ba27844ecef8d0285d5e691b9b54056cf
wistaria <synge.todo@gmail.com>
2013-07-18 14:25:22 +0900

    Added compile script in python

---
+++ packages/machikaneyama/compile.py
@@ -0,0 +1,52 @@
+#! /usr/bin/python
+
+# Compile script for Machikaneyama 2002 written by Synge Todo
+
+import sys
+import subprocess
+import os, os.path
+
+def CompileAndInstall(dir, file, prefix, compiler, option):
+    # Extract files from the tarball
+    print "Extracting", file + "..."
+    cmd = ['/usr/bin/tar', 'zxvf', file]
+    p = subprocess.check_call(cmd)
+
+    # Patch
+    print "Applying patch..."
+    patch = os.path.join(os.path.abspath(os.path.dirname(__file__)), dir + ".patch")
+    cmd = ['/usr/bin/patch', '-i', patch, '-p', '1', '-d', dir]
+    p = subprocess.check_call(cmd)
+    
+    # Make
+    cmd = ['/usr/bin/make', '-C', dir]
+    if (compiler):
+        cmd.append("fort=" + compiler)
+    if (option):
+        cmd.append("flag=" + option)
+    print "Compiling with", cmd, "..."
+    p = subprocess.check_call(cmd)
+
+    # Install
+    cmd = ['/usr/bin/make', '-C', dir]
+    if (prefix):
+        cmd.append("prefix=" + prefix)
+    cmd.append("install")
+    print "Installing with", cmd, "..."
+    p = subprocess.check_call(cmd)
+        
+if __name__ == '__main__':
+    dir = "cpa2002v009c"
+    file = dir + ".tar.gz"
+    if (len(sys.argv) < 2):
+        print "Usage:", sys.argv[0], "prefix [compiler] [option]"
+        sys.exit(127)
+    prefix = sys.argv[1]
+    compiler = ""
+    option = ""
+    if (len(sys.argv) >= 3):
+        compiler = sys.argv[2]
+    if (len(sys.argv) >= 4):
+        option = sys.argv[3]
+    ret = CompileAndInstall(dir, file, prefix, compiler, option)
+    sys.exit(ret)
--- packages/machikaneyama/download.py
+++ packages/machikaneyama/download.py
@@ -9,50 +9,52 @@
 import sys
 import mechanize
 
-url = "http://kkr.phys.sci.osaka-u.ac.jp/download.cgi"
-file = "cpa2002v009c.tar.gz"
+def Download(url, file, email, password):
+    # Create Browser
+    br = mechanize.Browser()
+    
+    # Browser options
+    br.set_handle_equiv(True)
+    br.set_handle_redirect(True)
+    br.set_handle_referer(True)
+    br.set_handle_robots(False)
+    
+    # Follows refresh 0 but not hangs on refresh > 0
+    br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
+    
+    # Open Machikaneyama download page
+    print "Connecting to " + url + "..."
+    res = br.open(url)
+    
+    # Fill form with email and password
+    print "Submitting email and password..."
+    br.select_form(nr = 0)
+    br["MAIL"] = email
+    br["PW"] = password
+    br.submit()
+    try:
+        req = br.click_link(text = file)
+    except mechanize._mechanize.LinkNotFoundError:
+        print "Invalid email and/or password"
+        return 127
+        
+    # Download tarball
+    print "Downloading " + file + "..."
+    br.open(req)
+    body = br.response().read()
+    f = open(file, 'w')
+    f.write(body)
+    f.close()
+    print "Done."
+    return 0
 
-if (len(sys.argv) != 3):
-    print "Usage:", sys.argv[0], "email", "password"
-    sys.exit(127)
-email = sys.argv[1]
-password = sys.argv[2]
-
-# Browser
-br = mechanize.Browser()
-
-# Browser options
-br.set_handle_equiv(True)
-br.set_handle_redirect(True)
-br.set_handle_referer(True)
-br.set_handle_robots(False)
-
-# Follows refresh 0 but not hangs on refresh > 0
-br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
-
-# Open Machikaneyama download page
-print "Connecting to", url, "..."
-res = br.open(url)
-
-# Fill form with email and password
-print "Submitting email and password ..."
-br.select_form(nr = 0)
-br["MAIL"] = email
-br["PW"] = password
-br.submit()
-try:
-    req = br.click_link(text = file)
-except mechanize._mechanize.LinkNotFoundError:
-    print "Invalid email and/or password"
-    sys.exit(127)
-
-# Download tarball
-print "Downloading", file, "..."
-br.open(req)
-body = br.response().read()
-f = open(file, 'w')
-f.write(body)
-f.close()
-
-print "Done"
-sys.exit(0)
+if __name__ == '__main__':
+    url = "http://kkr.phys.sci.osaka-u.ac.jp/download.cgi"
+    file = "cpa2002v009c.tar.gz"
+    if (len(sys.argv) != 3):
+        print "Usage:", sys.argv[0], "email", "password"
+        sys.exit(127)
+    email = sys.argv[1]
+    password = sys.argv[2]
+    ret = Download(url, file, email, password)
+    sys.exit(ret)
