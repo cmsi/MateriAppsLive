2949b273602c68f713f384624942a236578afccc
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2017-07-15 15:48:38 +0900

    Fixed for gem install in Jessie Changed materiapps repository URL Added script for removing deb from repository

---
+++ repos/remove.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+PACKAGE="$1"
+test -z $PACKAGE && exit 127
+echo "PACKAGE: $PACKAGE"
+
+CODENAME="jessie wheezy"
+for cname in $CODENAME; do
+  reprepro --ask-passphrase --ignore=wrongdistribution -Vb $HOME/data/apt/$cname remove $cname $PACKAGE
+done
