e800e0ad2c4401005d7b5c983c0322707ae8cd6b
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2017-10-30 11:21:46 +0900

    Add script for Debian 9 Stretch

--- vagrant-box/build-debian.sh
+++ vagrant-box/build-debian.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-SCRIPTS="build-debian7-amd64.sh build-debian7-i386.sh build-debian8-amd64.sh build-debian8-i386.sh"
+SCRIPTS="build-debian7-amd64.sh build-debian7-i386.sh build-debian8-amd64.sh build-debian8-i386.sh build-debian9-amd64.sh build-debian9-i386.sh"
 
 for s in $SCRIPTS; do
   sh $s
