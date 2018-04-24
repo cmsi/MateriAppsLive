dc00dfa23e786337d901a046d6da568f34cc2853
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2018-04-24 18:59:01 +0900

    Add user to vboxsf group

--- packer2/script/02_virtualbox.sh
+++ packer2/script/02_virtualbox.sh
@@ -1,10 +1,12 @@
 #!/bin/bash -eux
 
 echo "==> Installing VirtualBox guest additions"
-apt install -y linux-headers-$(uname -r) linux-headers-$(dpkg --print-architecture) dkms build-essential perl
+apt-get -y install linux-headers-$(uname -r) linux-headers-$(dpkg --print-architecture) dkms build-essential perl
 
 mount -o loop /home/user/VBoxGuestAdditions.iso /mnt
 sh /mnt/VBoxLinuxAdditions.run --nox11
 umount /mnt
 rm /home/user/VBoxGuestAdditions.iso
 rm /home/user/.vbox_version
+
+usermod -G vboxsf -a user
