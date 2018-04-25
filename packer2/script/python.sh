f6da39efdd1df8380c9e2d673a0c2ecb7ee0f33d
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2018-04-26 02:20:51 +0900

    add ipython2 and jupyterwq

--- packer2/script/python.sh
+++ packer2/script/python.sh
@@ -5,3 +5,15 @@ apt-get -y install python-pip python3-pip
 apt-get -y install python-virtualenv python3-venv
 apt-get -y install python-notebook jupyter-notebook
 apt-get -y install python-numpy python-scipy python-matplotlib python3-numpy python3-scipy python3-matplotlib
+
+# jupyter
+cat << EOF > /usr/local/bin/jupyter
+#!/usr/bin/python3
+from jupyter_core.command import main
+if __name__ == '__main__':
+    main()
+EOF
+chmod +x /usr/local/bin/jupyter
+
+# ipython2
+ln -s /usr/bin/ipython /usr/local/bin/ipython2
