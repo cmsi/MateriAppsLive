b6004f7006077c3ee80938361b48269afde3902f
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2019-02-19 16:49:04 +0900

    Update sync script

--- repos/host-sync.sh
+++ repos/host-sync.sh
@@ -2,20 +2,27 @@
 DEB_HOME=$HOME/vagrant/data
 RSYNC="rsync -avzP --delete -e ssh"
 
+DISTS="stretch jessie wheezy"
+
 set -x
-$RSYNC $DEB_HOME/apt/wheezy/dists $DEB_HOME/apt/wheezy/pool frs.sourceforge.net:/home/frs/project/materiappslive/Debian/apt
-$RSYNC $DEB_HOME/apt/wheezy/dists $DEB_HOME/apt/wheezy/pool frs.sourceforge.net:/home/frs/project/materiappslive/Debian/wheezy
-$RSYNC $DEB_HOME/apt/jessie/dists $DEB_HOME/apt/jessie/pool frs.sourceforge.net:/home/frs/project/materiappslive/Debian/jessie
-$RSYNC $DEB_HOME/apt/stretch/dists $DEB_HOME/apt/stretch/pool frs.sourceforge.net:/home/frs/project/materiappslive/Debian/stretch
+for d in $DISTS; do
+  $RSYNC $DEB_HOME/apt/$d/dists $DEB_HOME/apt/$d/pool root@exa.phys.s.u-tokyo.ac.jp:/var/www/vhosts/exa.phys.s.u-tokyo.ac.jp/httpdocs/archive/MateriApps/apt/$d
+done
+$RSYNC $DEB_HOME/archive/stretch/*.orig.tar.gz root@exa.phys.s.u-tokyo.ac.jp:/var/www/vhosts/exa.phys.s.u-tokyo.ac.jp/httpdocs/archive/MateriApps/src
+$RSYNC $(dirname $0)/sources root@exa.phys.s.u-tokyo.ac.jp:/var/www/vhosts/exa.phys.s.u-tokyo.ac.jp/httpdocs/archive/MateriApps
+$RSYNC $(dirname $0)/../README.md root@exa.phys.s.u-tokyo.ac.jp:/var/www/vhosts/exa.phys.s.u-tokyo.ac.jp/httpdocs/archive/MateriApps
+$RSYNC $(dirname $0)/../keys root@exa.phys.s.u-tokyo.ac.jp:/var/www/vhosts/exa.phys.s.u-tokyo.ac.jp/httpdocs/archive/MateriApps
+
+for d in $DISTS; do
+  $RSYNC $DEB_HOME/apt/$d/dists $DEB_HOME/apt/$d/pool frs.sourceforge.net:/home/frs/project/materiappslive/Debian/$d
+done
 $RSYNC $DEB_HOME/archive frs.sourceforge.net:/home/frs/project/materiappslive/Debian
 $RSYNC $(dirname $0)/sources frs.sourceforge.net:/home/frs/project/materiappslive/Debian/
 $RSYNC $(dirname $0)/../README.md frs.sourceforge.net:/home/frs/project/materiappslive
 $RSYNC $(dirname $0)/../keys frs.sourceforge.net:/home/frs/project/materiappslive/Debian/
 
-$RSYNC $DEB_HOME/apt/wheezy/dists $DEB_HOME/apt/wheezy/pool root@exa.phys.s.u-tokyo.ac.jp:/var/www/vhosts/exa.phys.s.u-tokyo.ac.jp/httpdocs/archive/MateriApps/apt/wheezy
-$RSYNC $DEB_HOME/apt/jessie/dists $DEB_HOME/apt/jessie/pool root@exa.phys.s.u-tokyo.ac.jp:/var/www/vhosts/exa.phys.s.u-tokyo.ac.jp/httpdocs/archive/MateriApps/apt/jessie
-$RSYNC $DEB_HOME/apt/stretch/dists $DEB_HOME/apt/stretch/pool root@exa.phys.s.u-tokyo.ac.jp:/var/www/vhosts/exa.phys.s.u-tokyo.ac.jp/httpdocs/archive/MateriApps/apt/stretch
-$RSYNC $DEB_HOME/archive/wheezy/*.orig.tar.gz root@exa.phys.s.u-tokyo.ac.jp:/var/www/vhosts/exa.phys.s.u-tokyo.ac.jp/httpdocs/archive/MateriApps/src
-$RSYNC $(dirname $0)/sources root@exa.phys.s.u-tokyo.ac.jp:/var/www/vhosts/exa.phys.s.u-tokyo.ac.jp/httpdocs/archive/MateriApps
-$RSYNC $(dirname $0)/../README.md root@exa.phys.s.u-tokyo.ac.jp:/var/www/vhosts/exa.phys.s.u-tokyo.ac.jp/httpdocs/archive/MateriApps
-$RSYNC $(dirname $0)/../keys root@exa.phys.s.u-tokyo.ac.jp:/var/www/vhosts/exa.phys.s.u-tokyo.ac.jp/httpdocs/archive/MateriApps
+for d in $DISTS; do
+  $RSYNC $DEB_HOME/apt/$d/dists $DEB_HOME/apt/$d/pool zetta:/var/www/html/Debian/$d
+done
+$RSYNC $DEB_HOME/archive zetta:/var/www/html/Debian
+$RSYNC $(dirname $0)/../keys zetta:/var/www/html/Debian
