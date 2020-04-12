f7916574e1969b6da6f3fa627e4de86c4bed923c
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2020-04-12 19:35:28 +0900

    Update base version

--- box/build-debian.sh
+++ box/build-debian.sh
@@ -3,6 +3,7 @@
 SCRIPT_DIR=$(cd "$(dirname $0)"; pwd)
 
 SCRIPTS="build-debian8-amd64.sh build-debian8-i386.sh build-debian9-amd64.sh build-debian9-i386.sh build-debian10-amd64.sh build-debian10-i386.sh"
+SCRIPTS="build-debian9-amd64.sh build-debian9-i386.sh build-debian10-amd64.sh build-debian10-i386.sh"
 
 for s in $SCRIPTS; do
   sh $SCRIPT_DIR/$s
