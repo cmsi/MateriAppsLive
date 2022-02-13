229ec23f1dbf7fed0be90b1078093d549fffaa5a
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2022-02-13 19:24:17 +0900

    working for ma4

--- packer/download.sh
+++ packer/download.sh
@@ -17,3 +17,16 @@ for arch in ${ARCHITECTURES}; do
     wget ${URL}
   fi
 done
+
+echo "DEBIAN11_VERSION=$DEBIAN11_VERSION"
+ARCHITECTURES="amd64"
+for arch in ${ARCHITECTURES}; do
+  ISO="debian-${DEBIAN11_VERSION}-${arch}-DVD-1.iso"
+  URL="http://cdimage.debian.org/cdimage/release/${DEBIAN11_VERSION}/${arch}/iso-dvd/${ISO}"
+  if [ -f "${ISO}" ]; then
+    echo "Waning: ${ISO} exists. Skip."
+  else
+    echo "Downloading ${URL}"
+    wget ${URL}
+  fi
+done
