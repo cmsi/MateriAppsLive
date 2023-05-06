e143c2cda467e3f92afceffe5bd2678315220ab9
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2023-05-06 18:04:15 +0900

    remove wheezy jessie xenial

--- repos/sources/setup.sh
+++ repos/sources/setup.sh
@@ -8,11 +8,7 @@ deb http://exa.phys.s.u-tokyo.ac.jp/archive/MateriApps/apt/${DEBIAN} ${DEBIAN} m
 deb-src http://exa.phys.s.u-tokyo.ac.jp/archive/MateriApps/apt/${DEBIAN} ${DEBIAN} main non-free contrib
 EOF
 
-if [ "${DEBIAN}" = "jessie" ]; then
-  apt-get update
-  apt-get -y --force-yes install materiapps-keyring
-else
-  apt-get -o Acquire::AllowInsecureRepositories=true update
-  apt-get -y --allow-unauthenticated install materiapps-keyring
+apt-get -o Acquire::AllowInsecureRepositories=true update
+apt-get -y --allow-unauthenticated install materiapps-keyring
 fi
 apt-get update
