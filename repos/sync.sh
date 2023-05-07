11012cf429bf792ead98962f8aad9edfc5cf5b6a
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2023-05-07 22:13:57 +0900

    update

--- repos/sync.sh
+++ repos/sync.sh
@@ -13,7 +13,7 @@ for d in $DISTS; do
 done
 $RSYNC $(dirname $0)/sources root@${EXA}:/var/www/html/archive/MateriApps
 
-for d in $DISTS; do
-  $RSYNC $DEB_HOME/apt/$d/dists $DEB_HOME/apt/$d/pool frs.sourceforge.net:/home/frs/project/materiappslive/Debian/$d
-done
+# for d in $DISTS; do
+#   $RSYNC $DEB_HOME/apt/$d/dists $DEB_HOME/apt/$d/pool frs.sourceforge.net:/home/frs/project/materiappslive/Debian/$d
+# done
 $RSYNC $(dirname $0)/sources frs.sourceforge.net:/home/frs/project/materiappslive/Debian/
