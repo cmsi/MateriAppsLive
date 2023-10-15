12e943f752f4a32c0db88f23ea616b14044d6853
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2023-10-15 12:23:25 +0900

    update

--- repos/list.sh
+++ repos/list.sh
@@ -3,5 +3,5 @@ PACKAGE="$1"
 test -z $PACKAGE && exit 127
 
 DATA_DIR=$HOME/malive/data
-CODENAME="bookworm bullseye buster stretch jessie wheezy jammy focal bionic xenial"
+CODENAME="bookworm bullseye buster stretch jammy focal bionic"
 for cname in $CODENAME; do reprepro -Vb $DATA_DIR/apt/$cname list $cname $PACKAGE; done
