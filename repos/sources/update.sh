665bf139d064d57379d69c37a6044e058989ae04
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2024-05-30 15:27:03 +0900

    update

--- repos/sources/update.sh
+++ repos/sources/update.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-rm -f /etc/apt/trusted.gpg.d/materiapps.gpg
+rm -f /etc/apt/trusted.gpg.d/materiapps.gpg /etc/apt/trusted.gpg.d/MateriApps_LIVE_Repository.gpg 
 curl -fsSL https://exa.phys.s.u-tokyo.ac.jp/archive/MateriApps/keys/materiapps.gpg > /etc/apt/trusted.gpg.d/materiapps.gpg
 chmod 644 /etc/apt/trusted.gpg.d/materiapps.gpg
 apt-get update
