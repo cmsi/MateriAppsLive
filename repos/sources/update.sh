7883fee4e34847e27356a219878654d38fc8a9d6
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2024-08-01 18:29:16 +0900

    add -k option

--- repos/sources/update.sh
+++ repos/sources/update.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 rm -f /etc/apt/trusted.gpg.d/materiapps.gpg /etc/apt/trusted.gpg.d/MateriApps_LIVE_Repository.gpg 
-curl -fsSL https://exa.phys.s.u-tokyo.ac.jp/archive/MateriApps/keys/materiapps.gpg > /etc/apt/trusted.gpg.d/materiapps.gpg
+curl -fksSL https://exa.phys.s.u-tokyo.ac.jp/archive/MateriApps/keys/materiapps.gpg > /etc/apt/trusted.gpg.d/materiapps.gpg
 chmod 644 /etc/apt/trusted.gpg.d/materiapps.gpg
 apt-get update
 apt-get -y install materiapps-keyring
