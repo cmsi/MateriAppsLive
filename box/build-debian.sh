917baa0381ab54198e65c54d09d4f7fb1041b18b
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2025-02-10 14:24:36 +0900

    add arm64 support

--- box/build-debian.sh
+++ box/build-debian.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
 SCRIPT_DIR=$(cd "$(dirname $0)"; pwd)
-
-SCRIPTS="build-debian11-amd64.sh build-debian12-amd64.sh"
+ARCH=$(arch)
+if [ "$ARCH" != "x86_64" ]; then
+  SCRIPTS="build-debian11-amd64.sh build-debian12-amd64.sh"
+else
+  SCRIPTS="build-debian11-arm64.sh build-debian12-arm64.sh"
+fi
 
 for s in $SCRIPTS; do
   sh $SCRIPT_DIR/$s
---
+++ box/build-debian11-arm64.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+SCRIPT_DIR=$(cd "$(dirname $0)"; pwd)
+. $SCRIPT_DIR/version.sh
+
+if [ -e "http" ]; then :; else ln -s $SCRIPT_DIR/http; fi
+if [ -e "script" ]; then :; else ln -s $SCRIPT_DIR/script; fi
+
+BOX="box/virtualbox/debian11-arm64-$DEBIAN11_VERSION.box"
+if [ -f "$BOX" ]; then
+    echo "Warning: $BOX exists. Skip to build box"
+else
+  sed -e "s|@DEBIAN11_VERSION@|${DEBIAN11_VERSION}|g" \
+      -e "s|@DEBIAN11_CHECKSUM@|${DEBIAN11_ARM64_CHECKSUM}|g" \
+      $SCRIPT_DIR/debian11-arm64.json.in > debian11-arm64.json
+  mkdir -p log
+  packer build -only=virtualbox-iso -var-file=debian11-arm64.json $SCRIPT_DIR/debian.json 2>&1 | tee log/build-debian11-arm64.log
+fi
+if [ -f "$BOX" ]; then
+  if [ -f /usr/bin/md5sum ]; then
+    CHECKSUM=$(md5sum $BOX | cut -d ' ' -f 1)
+  else
+    CHECKSUM=$(md5 $BOX | cut -d ' ' -f 4)
+  fi
+  sed -e "s|@DEBIAN11_VERSION@|${DEBIAN11_VERSION}|g" \
+      -e "s|@CHECKSUM@|${CHECKSUM}|g" \
+  $SCRIPT_DIR/debian11-arm64.metadata.in > box/virtualbox/debian11-arm64.json
+fi
