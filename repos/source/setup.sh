5591413596b9a544eb914ad5489e07e556c22407
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2025-02-10 16:57:41 +0900

    update

--- repos/source/setup.sh
+++ repos/source/setup.sh
@@ -4,7 +4,9 @@ ROOT=https://exa.phys.s.u-tokyo.ac.jp/archive/MateriApps/apt
 DEBIAN=$(lsb_release -c -s)
 ARCH=$(dpkg --print-architecture)
 
+dpkg --purege materiapps-keyring > /dev/null 2>&1
 rm -f /etc/apt/trusted.gpg.d/materiapps.gpg /etc/apt/trusted.gpg.d/MateriApps_LIVE_Repository.gpg 
+rm -f /etc/apt/sources.list.d/materiapps-*.list
 
 cat << EOF > /etc/apt/sources.list.d/materiapps.list
 # for MateriApps LIVE!
